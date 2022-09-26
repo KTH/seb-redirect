187ff2e47900b2cc896de7d39eec3093cc4cf193
Sebastian Ware <sebastian@urbantalk.se>
2022-09-26 14:56:40 +0200

    Remove dual prefixes

--- routes/redirectRoutes.js
+++ routes/redirectRoutes.js
@@ -3,12 +3,12 @@ const express   = require('express')
 const router    = express.Router({mergeParams: true});
 
 //index route
-router.get(proxyPath(''), function(req, res){
+router.get('', function(req, res){
   res.render('index');
 });
 
 //redirect route
-router.get(proxyPath('redirect'), function(req, res){
+router.get('redirect', function(req, res){
   res.render('redirect', { redirectURL: 'https://canvas.kth.se/courses/'+req.query.cid });
 });
 
