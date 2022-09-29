98a22b8575863679796db2cc273f13a8278a2c11
Sebastian Ware <sebastian@urbantalk.se>
2022-09-29 10:42:31 +0200

    Remove unused file

--- routes/utils.js
+++
@@ -1,9 +0,0 @@
-const PROXY_PATH_PREFIX = process.env.PROXY_PATH_PREFIX || '/';
-
-module.exports.proxyPath = (enpointPath) => {
-    let sep="/";
-    if (PROXY_PATH_PREFIX.endsWith("/") || enpointPath.startsWith("/")) {
-        sep=""
-    }
-    return [PROXY_PATH_PREFIX, sep, enpointPath].join("");
-}
